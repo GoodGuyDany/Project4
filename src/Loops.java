5922314ecac540a6d57bac20c0794c93413c78f4
GoodGuyDany <mdsdanylo@gmail.com>
2022-09-22 13:20:04 +0300

    Commit2 with fixed part 3

--- src/Loops.java
+++ src/Loops.java
@@ -69,78 +69,78 @@ public class Loops {
 //                o++;
 //            }
 //        }
-// Part 3 (Didn't work for me, have no idea why)
-//        System.out.println("Hello, User! Can you please input a String for me?");
-//        String string3 = sc.nextLine();
-//        int count1 = string3.length();
-//        for (int p = count1; p >= 0; p--){
-//            System.out.println(string3.charAt(p));
-//        }
-// Part 4
-        String newline = System.lineSeparator();
-        boolean h = true;
-        while (h = true) {
-            System.out.println("Hello, User, what's your name?");
-            String username = sc.nextLine();
-            System.out.println("Nice to meet you, " + username + ". What's your favorite movie genre?");
-            System.out.println("Here are some of the most popular genres: " + newline + "Horror" + newline + "Fantasy" + newline + "Sci-Fi" + newline + "Thriller" + newline + "Comedy");
-            String genre = sc.nextLine();
-            genre = genre.toUpperCase();
-            System.out.println("And one more question: how old are you?");
-            int userage = sc.nextInt();
-            if (userage <= 15) {
-                switch (genre) {
-                    case "HORROR":
-                        System.out.println("I wouldn't recommend you watching any horrors until 16.");
-                        break;
-                    case "FANTASY":
-                        System.out.println("Charlie and the Chocolate Factory is a cool fantasy movie!");
-                        break;
-                    case "SCI-FI":
-                        System.out.println("Buzz Lightyear is a cool sci-fi movie to watch!");
-                        break;
-                    case "THRILLER":
-                        System.out.println("Nightmare before Christmas is a cool thriller movie to watch!");
-                        break;
-                    case "COMEDY":
-                        System.out.println("Bad Guys is a cool comedy movie to watch!");
-                        break;
-                    default:
-                        System.out.println("");
-                }
-            } else if (userage >= 16) {
-                switch (genre) {
-                    case "HORROR":
-                        System.out.println("Time is a cool horror movie to watch!");
-                        break;
-                    case "FANTASY":
-                        System.out.println("Doctor Strange is a cool fantasy movie!");
-                        break;
-                    case "SCI-FI":
-                        System.out.println("Samaritan is a cool sci-fi movie to watch!");
-                        break;
-                    case "THRILLER":
-                        System.out.println("Deep Waters is a cool thriller movie to watch!");
-                        break;
-                    case "COMEDY":
-                        System.out.println("Man from Toronto is a cool comedy movie to watch!");
-                        break;
-                    default:
-                        System.out.println("");
-                }
-            }
-            sc.nextLine();
-            System.out.println("Would you like to try again?");
-            String answer5 = sc.nextLine();
-            answer5 = answer5.toUpperCase();
-            if (answer5.equals("Y")){
-                System.out.println("Sure thing!");
-            }
-            else{
-                System.out.println("Oh, alright!");
-                System.exit(0);
-            }
-
+// Part 3
+        System.out.println("Hello, User! Can you please input a String for me?");
+        String string3 = sc.nextLine();
+        int count1 = string3.length();
+        for (int p = count1-1; p >= 0; p--){
+            System.out.print(string3.charAt(p));
         }
+// Part 4
+//        String newline = System.lineSeparator();
+//        boolean h = true;
+//        while (h = true) {
+//            System.out.println("Hello, User, what's your name?");
+//            String username = sc.nextLine();
+//            System.out.println("Nice to meet you, " + username + ". What's your favorite movie genre?");
+//            System.out.println("Here are some of the most popular genres: " + newline + "Horror" + newline + "Fantasy" + newline + "Sci-Fi" + newline + "Thriller" + newline + "Comedy");
+//            String genre = sc.nextLine();
+//            genre = genre.toUpperCase();
+//            System.out.println("And one more question: how old are you?");
+//            int userage = sc.nextInt();
+//            if (userage <= 15) {
+//                switch (genre) {
+//                    case "HORROR":
+//                        System.out.println("I wouldn't recommend you watching any horrors until 16.");
+//                        break;
+//                    case "FANTASY":
+//                        System.out.println("Charlie and the Chocolate Factory is a cool fantasy movie!");
+//                        break;
+//                    case "SCI-FI":
+//                        System.out.println("Buzz Lightyear is a cool sci-fi movie to watch!");
+//                        break;
+//                    case "THRILLER":
+//                        System.out.println("Nightmare before Christmas is a cool thriller movie to watch!");
+//                        break;
+//                    case "COMEDY":
+//                        System.out.println("Bad Guys is a cool comedy movie to watch!");
+//                        break;
+//                    default:
+//                        System.out.println("");
+//                }
+//            } else if (userage >= 16) {
+//                switch (genre) {
+//                    case "HORROR":
+//                        System.out.println("Time is a cool horror movie to watch!");
+//                        break;
+//                    case "FANTASY":
+//                        System.out.println("Doctor Strange is a cool fantasy movie!");
+//                        break;
+//                    case "SCI-FI":
+//                        System.out.println("Samaritan is a cool sci-fi movie to watch!");
+//                        break;
+//                    case "THRILLER":
+//                        System.out.println("Deep Waters is a cool thriller movie to watch!");
+//                        break;
+//                    case "COMEDY":
+//                        System.out.println("Man from Toronto is a cool comedy movie to watch!");
+//                        break;
+//                    default:
+//                        System.out.println("");
+//                }
+//            }
+//            sc.nextLine();
+//            System.out.println("Would you like to try again?");
+//            String answer5 = sc.nextLine();
+//            answer5 = answer5.toUpperCase();
+//            if (answer5.equals("Y")){
+//                System.out.println("Sure thing!");
+//            }
+//            else{
+//                System.out.println("Oh, alright!");
+//                System.exit(0);
+//            }
+//
+//        }
     }
 }
